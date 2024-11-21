2f1e6aaa20fac3c2f6c4293be3dfb2aab9deaf69
Ahmet Salih Gökdağ <52855724+senzoyen@users.noreply.github.com>
2024-11-21 12:48:42 +0300

    Add files via upload

--- TUSMEC-Arayüz/tusmec_interface/map_app/views.py
+++ TUSMEC-Arayüz/tusmec_interface/map_app/views.py
@@ -9,20 +9,25 @@ from .models import Coordinate
 from .serializer import CoordinateSerializer
 
 # Arduino bağlantısını başlatıyoruz (port numarasını kendi sisteminize göre ayarlayın)
-#arduino = serial.Serial('COM7', 9600, timeout=1)
+arduino = serial.Serial('COM5', 9600, timeout=1)
 
 @api_view(['POST'])
 def save_coordinate(request):
     # İstekten gelen JSON formatındaki koordinatları alıyoruz
     coordinates = json.loads(request.POST.get('coordinates', '[]'))
     
+    selected_mode = request.data.get('selectedMode', 'default')
+
+    print(f"Seçilen Mod: {selected_mode}")
+    
     # Koordinatları LoRa üzerinden göndermek için formatlıyoruz ve gönderiyoruz
     for coord in coordinates:
         print(f"Latitude: {coord['latitude']}, Longitude: {coord['longitude']}")
         
         # Koordinatları Arduino’ya göndermek için CSV formatına çeviriyoruz
-    #    message = f"{coord['latitude']},{coord['longitude']}\n"
- #       arduino.write(message.encode())  # Veriyi Arduino'ya gönderiyoruz
+        message = f"{coord['latitude']},{coord['longitude']}\n"
+        arduino.write(message.encode())  # Veriyi Arduino'ya gönderiyoruz
+        print("gönderildi")
         time.sleep(1)  # Arduino'nun veriyi işleyebilmesi için bekliyoruz
     
     # Koordinatları veritabanına kaydetmek için serializer'ı kullanıyoruz
